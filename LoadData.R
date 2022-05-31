7fabc5d10a95697ab6e2b384c87b8d86b99c988b
AAdnansevinc <72599641+AAdnansevinc@users.noreply.github.com>
2022-05-31 23:10:02 +0200

    Update LoadData.R

--- LoadData.R
+++ LoadData.R
@@ -5,24 +5,26 @@
 
 # Set Directory
 
-setwd("C:\\Users\\Adnan_Sevinc\\OneDrive - EPAM\\University\\2.Semester\\Advanced Programming in R 2400-DS1APR\\Project\\data")
+setwd("")
 getwd()
 
 # Data set by country
 poland <- read.csv("poland economic indicator from world data bank.csv")
 
 # Data set by city
-expenditure_education <- read.csv2("Expenditure per capita for education by city .csv")
-expenditure_total <- read.csv2("Expenditure per capita grand total by city.csv")
-average_salary <- read.csv2("Average monthly gross wages and salaries by city.csv")
-academic_teacher <- read.csv2("Academic teachers by city.csv")
-consumption_electricity <- read.csv2("Consumption of electricity by economic sectors by city.csv")
-prices_index_consumer_good <- read.csv2("Price indices of consumer goods and services by city.csv")
-unemployment <- read.csv2("Registered unemployment rate by city.csv")
-retail_sale <- read.csv2("Retail sales of goods by city .csv")
-revenue_per_capita <- read.csv2("Revenue per capita by city.csv")
-road_accident <- read.csv2("Road traffic accidents; Years by city.csv")
-urban_population <-read.csv2("Urban population in % of total population (half-yearly data) by city.csv")
+expenditure_education <- read.csv2("Expenditure per capita for education by city .csv", fileEncoding = "UTF-8")
+expenditure_total <- read.csv2("Expenditure per capita grand total by city.csv", fileEncoding = "UTF-8")
+average_salary <- read.csv2("Average monthly gross wages and salaries by city.csv",fileEncoding = "UTF-8")
+academic_teacher <- read.csv2("Academic teachers by city.csv",fileEncoding = "UTF-8")
+consumption_electricity <- read.csv2("Consumption of electricity by economic sectors by city.csv",fileEncoding = "UTF-8")
+prices_index_consumer_good <- read.csv2("Price indices of consumer goods and services by city.csv",fileEncoding = "UTF-8")
+unemployment <- read.csv2("Registered unemployment rate by city.csv",fileEncoding = "UTF-8")
+retail_sale <- read.csv2("Retail sales of goods by city .csv",fileEncoding = "UTF-8")
+revenue_per_capita <- read.csv2("Revenue per capita by city.csv",fileEncoding = "UTF-8")
+road_accident <- read.csv2("Road traffic accidents; Years by city.csv",fileEncoding = "UTF-8")
+urban_population <-read.csv2("Urban population in % of total population (half-yearly data) by city.csv",fileEncoding = "UTF-8")
+
+
 
 ##################
 # Data Preparation
@@ -130,6 +132,4 @@ for (i in 1:length(colnames(poland))){
 }
 
 poland$Time.Code <- NULL
-
-
 
