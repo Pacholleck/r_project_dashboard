e838f57c5ded648c5bbba6e152c7dcc728f69043
AAdnansevinc <72599641+AAdnansevinc@users.noreply.github.com>
2022-06-23 23:07:19 +0200

    Update dashboard_loadData.R

--- dashboard_loadData.R
+++ dashboard_loadData.R
@@ -13,7 +13,7 @@
   average_salary <- read.csv2("data/Average monthly gross wages and salaries by city.csv",fileEncoding = "UTF-8")
   #academic_teacher <- read.csv2("data/Academic teachers by city.csv",fileEncoding = "UTF-8")
   consumption_electricity <- read.csv2("data/Consumption of electricity by economic sectors by city.csv",fileEncoding = "UTF-8")
-  prices_index_consumer_good <- read.csv2("data/Price indices of consumer goods and services by city.csv",fileEncoding = "UTF-8")
+  CPI <- read.csv2("data/Price indices of consumer goods and services by city.csv",fileEncoding = "UTF-8")
   unemployment <- read.csv2("data/Registered unemployment rate by city.csv",fileEncoding = "UTF-8")
   retail_sale <- read.csv2("data/Retail sales of goods by city .csv",fileEncoding = "UTF-8")
   revenue_per_capita <- read.csv2("data/Revenue per capita by city.csv",fileEncoding = "UTF-8")
@@ -61,23 +61,22 @@
   )
   
   
-  for (i in 1:length(colnames(poland))){
+  for (i in 1:length(colnames(poland))){    #Rename columns
     colnames(poland)[i] <- polcolnames[i]
   }
   
-  poland$Time.Code <- NULL
+  poland$Time.Code <- NULL   #Drop Code Column
   
-  poland$Time <- as.Date(paste(poland$Time, 1, 1, sep = "-"))
+  poland$Time <- as.Date(paste(poland$Time, 1, 1, sep = "-"))  #set data type
             
-  lastUpdate <- max(poland$Time)
   
-  for (i in 4:length(colnames(poland))){
+  for (i in 4:length(colnames(poland))){      #converting to numeric
     poland[i] = as.numeric(poland[,i])
   }
   
-  reg_data <-na.omit(poland)
+  reg_data <-na.omit(poland)  #Drop NA
   
-  for (i in 4:length(colnames(reg_data))){
+  for (i in 4:length(colnames(reg_data))){  #Taking Logarithmic
     reg_data[i] = log(reg_data[,i])
   }
   
@@ -87,6 +86,7 @@
   ########Voivodships########
   ###########################
   
+  # Remove specific columns
   average_salary <- 
     average_salary %>% 
     select(-c(grand.total.2021..PLN.))
@@ -100,19 +100,19 @@
     consumption_electricity %>% 
     select(-c(total.2021..GWh.))
   
+  #Subset CPI into different dataframes
+  CPI_total <- CPI[,1:19]
+  CPI_food <- CPI[,c(1,2,seq(20,36))]
+  CPI_alc <- CPI[,c(1,2,seq(37,53))]
+  CPI_clothing <- CPI[,c(1,2,seq(54,70))]
+  CPI_dwelling <- CPI[,c(1,2,seq(71,87))]
+  CPI_health <- CPI[,c(1,2,seq(88,104))]
+  CPI_transport <- CPI[,c(1,2,seq(105,121))]
+  CPI_recreation <- CPI[,c(1,2,seq(122,138))]
+  CPI_education <- CPI[,c(1,2,seq(139,155))]
   
-  prices_index_consumer_good_total <- prices_index_consumer_good[,1:19]
-  prices_index_consumer_good_food <- prices_index_consumer_good[,c(1,2,seq(20,36))]
-  prices_index_consumer_good_alc <- prices_index_consumer_good[,c(1,2,seq(37,53))]
-  prices_index_consumer_good_clothing <- prices_index_consumer_good[,c(1,2,seq(54,70))]
-  prices_index_consumer_good_dwelling <- prices_index_consumer_good[,c(1,2,seq(71,87))]
-  prices_index_consumer_good_health <- prices_index_consumer_good[,c(1,2,seq(88,104))]
-  prices_index_consumer_good_transport <- prices_index_consumer_good[,c(1,2,seq(105,121))]
-  prices_index_consumer_good_recreation <- prices_index_consumer_good[,c(1,2,seq(122,138))]
-  prices_index_consumer_good_education <- prices_index_consumer_good[,c(1,2,seq(139,155))]
   
-  
-  
+  # Dataframes to preprocess
   dataframes <- list(#academic_teacher=academic_teacher,
                      average_salary=average_salary,
                      consumption_electricity=consumption_electricity,
@@ -122,19 +122,20 @@
                      retail_sale=retail_sale,
                      road_accident = road_accident,
                      revenue_per_capita = revenue_per_capita,
-                     prices_index_consumer_good_total=prices_index_consumer_good_total,
-                     prices_index_consumer_good_food =prices_index_consumer_good_food,
-                     prices_index_consumer_good_alc=prices_index_consumer_good_alc,
-                     prices_index_consumer_good_education = prices_index_consumer_good_education,
-                     prices_index_consumer_good_recreation = prices_index_consumer_good_recreation,
-                     prices_index_consumer_good_transport = prices_index_consumer_good_transport,
-                     prices_index_consumer_good_dwelling=prices_index_consumer_good_dwelling,
-                     prices_index_consumer_good_clothing=prices_index_consumer_good_clothing
+                     CPI_total=CPI_total,
+                     CPI_food =CPI_food,
+                     CPI_alc=CPI_alc,
+                     CPI_education = CPI_education,
+                     CPI_recreation = CPI_recreation,
+                     CPI_transport = CPI_transport,
+                     CPI_dwelling=CPI_dwelling,
+                     CPI_clothing=CPI_clothing
                      
                      
                      
                      )
   
+  # Basic data cleaning
   basic_clean <- function(dataset){
     col_num <- as.character(c(seq(2005,2021)))
     if("X" %in% colnames(dataset) == TRUE) {dataset %>% select(-X)}
@@ -146,16 +147,16 @@
   
   dataframes <- lapply(dataframes, basic_clean) 
   
-
-
-  
+  # Melt precleaned data
   dataframes <- lapply(dataframes,function(x) melt(x, id = c('Code','Voivodeship'),variable.name = "Year"))
   
+  # Change columnnames
   df_names <- names(dataframes)
   for (i in 1:length(df_names)){
     colnames(dataframes[[i]]) <- c('Code', 'Voivodeship','Year',df_names[i])
   }
 
+  # Reduce nested list into one dataframe
   df_Voivodeship <- Reduce(function(x, y) merge(x, y, all=TRUE), dataframes)
   df_Voivodeship$Year <- as.numeric(as.character(df_Voivodeship$Year))
   
